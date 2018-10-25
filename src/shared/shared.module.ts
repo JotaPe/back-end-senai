748629393d13efe858268af8d8d716a07423c2f7
Jotape <jotape@firemail.cc>
2018-10-25 15:58:12 -0300

    changed structure

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { Module, Global } from '@nestjs/common';
-import { ConfigurationService } from './configuration/configuration.service';
 import { MapperService } from './mapper/mapper.service';
-import { DatabaseService } from './database/database.service';
 
 @Global()
 @Module({
-  providers: [ConfigurationService, MapperService, DatabaseService]
+  providers: [MapperService],
+  exports: [MapperService],
 })
 export class SharedModule {}
