024b707f24736c7f8d1776f9da307872b0483abd
Jotape <jotape@firemail.cc>
2018-11-27 13:37:56 -0200

    added pipes and added service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,25 @@
-import { UserService } from './user.service';
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UsePipes } from '@nestjs/common';
+
+import { ValidationPipe } from './../shared/validation.pipe';
 import { UserDTO } from './user.dto';
+import { UserService } from './user.service';
 
 @Controller('api/user')
 export class UserController {
   constructor(private userService: UserService) {}
   @Get('all')
-  showAllUsers() {
-    this.userService.showAll();
+  showAllUsers(@Query('page') page: number) {
+    return this.userService.showAll(page);
   }
 
   @Post('login')
+  @UsePipes(new ValidationPipe())
   login(@Body() data: UserDTO) {
     this.userService.login(data);
   }
 
   @Post('register')
+  @UsePipes(new ValidationPipe())
   register(@Body() data: UserDTO) {
     this.userService.register(data);
   }
