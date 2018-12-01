5ba78c4b953e50f3c63a2903024c81769ea55b98
Jotape <jotape@firemail.cc>
2018-12-01 19:56:39 -0200

    trying to reimplement

--- src/warning/warning.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('warning')
-export class WarningController {}
--- src/warning/warning.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WarningController } from './warning.controller';
-import { WarningService } from './warning.service';
-
-@Module({
-  controllers: [WarningController],
-  providers: [WarningService]
-})
-export class WarningModule {}
