e0eb6e59458e0193ddff963cf5e05eacce7611e1
Jotape <jotape@firemail.cc>
2018-12-01 19:56:29 -0200

    fixed bug

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { ExerciseEntity } from '../exercise/exercise.entity';
+import { ExerciseEntity } from './../exercise/exercise.entity';
+import { ExerciseService } from './../exercise/exercise.service';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
+import { TeacherModule } from './teacher/teacher.module';
+import { StudentModule } from './student/student.module';
+import { AdministrationModule } from './administration/administration.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, ExerciseEntity])],
-  providers: [UserService],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, ExerciseEntity]),
+    TeacherModule,
+    StudentModule,
+    AdministrationModule,
+  ],
   controllers: [UserController],
+  providers: [UserService, ExerciseService],
 })
 export class UserModule {}
