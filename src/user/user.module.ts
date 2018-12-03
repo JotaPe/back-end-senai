5421817c334197177092caabb1ee73675ef21cc0
Jotape <jotape@firemail.cc>
2018-12-03 07:38:00 -0200

    removed imports from the testing modules

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,9 +5,6 @@ import { ExerciseService } from './../exercise/exercise.service';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
-import { TeacherModule } from './teacher/teacher.module';
-import { StudentModule } from './student/student.module';
-import { AdministrationModule } from './administration/administration.module';
 
 @Module({
   imports: [
