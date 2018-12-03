7b37b0189736834a3c4c0045f7ee7a3c799207c7
Jotape <jotape@firemail.cc>
2018-12-03 06:41:28 -0200

    added module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccessControlModule } from 'nest-access-control';
 import { ConfigModule, InjectConfig } from 'nestjs-config';
-
 import { roles } from './app.roles';
 import { ExerciseModule } from './exercise/exercise.module';
 import { HttpErrorFilter } from './shared/http-error.filter';
 import { LoggingInterceptor } from './shared/logging.interceptor';
 import { UserModule } from './user/user.module';
+import { WarningModule } from './warning/warning.module';
 
 @Module({
   imports: [
@@ -22,6 +22,7 @@ import { UserModule } from './user/user.module';
     AccessControlModule.forRoles(roles),
     ExerciseModule,
     UserModule,
+    WarningModule,
   ],
   providers: [
     {
@@ -46,7 +47,7 @@ export class AppModule {
   constructor(@InjectConfig() private readonly config) {
     AppModule.projectName = config.get('base.name');
     AppModule.version = config.get('base.version');
-    AppModule.port = config.get('base.port') || 3000;
+    AppModule.port = config.get('base.port') || process.env.PORT;
     AppModule.host = config.get('base.host') || 'localhost';
     AppModule.isDev = config.get('base.env') || 'development';
     AppModule.tag = config.get('base.tags');
